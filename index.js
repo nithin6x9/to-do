d5f408afb299bd12b0c2dd5d720919f3dcacfb0b
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-07 22:02:13 +0530

    express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post('/todo',function(req,res){
+
+})
+
+app.get("/todo",function(req,res){
+
+})
+
+app.pust("/completed",function(req,res){
+    
+})
