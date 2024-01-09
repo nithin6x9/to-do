7f7c500c07a9e10e0448f142bf253d76abf8c7e5
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-10 00:18:58 +0530

    up

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,8 +1,14 @@
-export function Todos(){
-    return 
-    <div>
-    <h1>Go to gym</h1>
-    <h2>You need to go to the gym</h2>
-    <button>Mark as completed</button>
+export function Todos({todos}){
+    
+    return <div>
+    {
+        todos.map(function(todo){    
+        return <div>
+        <h1>{todo.title}</h1>
+        <h2>{todo.description}</h2>
+        <button>{todo.status == true ? "completed":"Mark as completed"}</button>
+        </div>
+    
+    })}
     </div>
 }
